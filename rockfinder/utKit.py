7a270f8d3b1ffa0f95380480424a8de37b4b5ac1
thespacedoctor <nothingbutdave@gmail.com>
2016-03-02 14:02:32 +0000

    first commit

---
+++ rockfinder/utKit.py
@@ -0,0 +1,14 @@
+from fundamentals import utKit
+
+# OVERRIDES
+
+
+class utKit(utKit):
+
+    """
+    Override dryx utKit
+    """
+    # Variable Data Atrributes
+
+    # Override Variable Data Atrributes
+    # xdpc-override-utkit-init
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+
+moduleDirectory = os.path.dirname(os.path.realpath(__file__))
+exec(open(moduleDirectory + "/rockfinder/__version__.py").read())
+
+
+def readme():
+    with open(moduleDirectory + '/README.md') as f:
+        return f.read()
+
+
+setup(name='rockfinder',
+      version=__version__,
+      description='CL-util to check the Minor Planet Centre's MPChecker for moving objects at a given sky location and epoch. Returns either the name of the closest object, or a non-match.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities',
+      ],
+      keywords=['terminal', 'astronomy','tool'],
+      url='https://github.com/thespacedoctor/rockfinder',
+      download_url='https://github.com/thespacedoctor/rockfinder/archive/v%(__version__)s.zip' % locals(
+      ),
+      author='David Young',
+      author_email='davidrobertyoung@gmail.com',
+      license='MIT',
+      packages=['rockfinder'],
+      install_requires=[
+          'pyyaml',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      # entry_points={
+      #     'console_scripts': ['rockfinder=rockfinder.cl_utils:main'],
+      # },
+      zip_safe=False)
